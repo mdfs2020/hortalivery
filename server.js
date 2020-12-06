f53cb073ecf83a85323294e490f30f506d02cf44
mdfs2020 <moniquedalia0@gmail.com>
2020-12-06 11:46:02 -0300

    Alteração nas strings de conexão

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log(`app está rodando na porta ${port}`);
 });
